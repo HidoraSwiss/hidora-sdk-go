3935321251d957eb9ffab7582650d4958a1e732e
Antonin Sarrazin <antonin.sarrazin@etik.com>
2023-01-16 21:46:02 +0100

    Define HidoraClient type and implement all locality of Hidora provider

--- hidora/client.go
+++ hidora/client.go
@@ -3,10 +3,70 @@ package hidora
 import (
 	"net/http"
 	"net/url"
+	"time"
 )
 
-type Client struct {
-	BaseUrl    *url.URL
-	HTTPClient *http.Client
-	Token      string
+type HidoraAuth string
+type HidoraEnvGroup string
+
+// type HidoraRegion string
+// type HidoraZone string
+
+type HidoraClient struct {
+	httpClient      *http.Client
+	auth            HidoraAuth
+	apiUrl          *url.URL
+	apiVersion      string
+	userAgent       string
+	defaultEnvGroup HidoraEnvGroup
+	defaultRegion   HidoraRegion
+	defaultZone     HidoraZone
+}
+
+const HIDORA_HOST_URL string = "app.hidora.com"
+const HIDORA_CLIENT_TIMEOUT time.Duration = 1800 // 30 minutes
+
+func NewClient(auth HidoraAuth,
+	host string,
+	apiVersion string,
+	defaultEnvGroup HidoraEnvGroup,
+	defaultRegion HidoraRegion,
+	defaultZone HidoraZone) (*HidoraClient, error) {
+	var hidoraClient = defaultparams()
+	if host != "" {
+		hidoraClient.apiUrl.Host = host
+	}
+	if apiVersion != "" {
+		hidoraClient.apiVersion = apiVersion
+		hidoraClient.apiUrl.Path = "/" + apiVersion + "/"
+	}
+	if defaultEnvGroup != "" {
+		hidoraClient.defaultEnvGroup = defaultEnvGroup
+	}
+	if defaultRegion != "" {
+		HidoraClient.defaultRegion = defaultRegion
+	}
+	if defaultZone != "" {
+		HidoraClient.defaultZone = defaultZone
+	}
+	return hidoraClient, nil
+}
+
+func defaultparams() *HidoraClient {
+	return &HidoraClient{
+		httpClient: &http.Client{
+			Timeout: time.Second * HIDORA_CLIENT_TIMEOUT,
+		},
+		auth: "",
+		apiUrl: &url.URL{
+			Scheme: "https",
+			Host:   HIDORA_HOST_URL,
+			Path:   "/1.0/",
+		},
+		apiVersion:      "1.0",
+		userAgent:       "Golang_Hidora_SDK/1.0",
+		defaultEnvGroup: "Hidora_EnvGroup",
+		defaultRegion:   "ch-gen",
+		defaultZone:     "ch-gen-1",
+	}
 }
--- hidora/locality.go
+++ hidora/locality.go
@@ -1,25 +1,25 @@
 package hidora
 
-// Zone is an availability zone
-type Zone string
-
-const (
-	// ZoneChGen1 represents the ch-gen-1 zone
-	ZoneChGen1 = Zone("ch-gen-1")
-	// ZoneChVd1 represents the ch-vd-1 zone
-	ZoneChVd1 = Zone("ch-vd-1")
-	// ZoneChVd2 represents the ch-vd-2 zone
-	ZoneChVd2 = Zone("ch-vd-2")
-	// ZoneChVdTrial represents the ch-vd-trial zone
-	ZoneChVdTrial = Zone("ch-vd-trial")
-)
-
 // Region is a geographical location
-type Region string
+type HidoraRegion string
 
 const (
 	// RegionChGen represents the ch-gen region
-	RegionChGen = Region("ch-gen")
+	RegionChGen = HidoraRegion("ch-gen")
 	// RegionChVd represents the ch-vd region
-	RegionChVd = Region("ch-vd")
+	RegionChVd = HidoraRegion("ch-vd")
+)
+
+// Zone is an availability zone
+type HidoraZone string
+
+const (
+	// ZoneChGen1 represents the ch-gen-1 zone
+	ZoneChGen1 = HidoraZone("ch-gen-1")
+	// ZoneChVd1 represents the ch-vd-1 zone
+	ZoneChVd1 = HidoraZone("ch-vd-1")
+	// ZoneChVd2 represents the ch-vd-2 zone
+	ZoneChVd2 = HidoraZone("ch-vd-2")
+	// ZoneChVdTrial represents the ch-vd-trial zone
+	ZoneChVdTrial = HidoraZone("ch-vd-trial")
 )
